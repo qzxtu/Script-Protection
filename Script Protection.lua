5787a93ea9f287dcfe9143b1e9c48b39b35dfa4e
Nova <69091361+qzxtu@users.noreply.github.com>
2023-05-06 16:02:10 -0500

    Update Script Protection.lua

--- Script Protection.lua
+++ Script Protection.lua
@@ -1,37 +1,37 @@
 -- Load the script from the given URL
-local url = "Script Link Here"
-local script = game:HttpGet(url, true) -- Use HTTPS
+local scriptUrl = "Script Link Here"
+local scriptContent = game:HttpGet(scriptUrl, true) -- Use HTTPS
 
 -- Record the start time for performance measurement
 local startTime = os.clock()
 
 -- Convert the script to bytecode
-local function dumpString(str)
-    local dumped = string.dump(loadstring(str))
-    return dumped
+local function convertStringToBytecode(str)
+    local bytecode = string.dump(loadstring(str))
+    return bytecode
 end
-local bytecode = dumpString(script)
+local scriptBytecode = convertStringToBytecode(scriptContent)
 
 -- Escape all characters in the script
-local function escapeString(str)
-    local escaped = ""
+local function escapeAllCharactersInString(str)
+    local escapedString = ""
     for i = 1, #str do
-        escaped = escaped .. "\\" .. string.byte(str, i)
+        escapedString = escapedString .. "\\" .. string.byte(str, i)
     end
-    return escaped
+    return escapedString
 end
-local escapedBytecode = escapeString(bytecode)
+local escapedScriptBytecode = escapeAllCharactersInString(scriptBytecode)
 
 -- Encrypt the escaped bytecode using a secret key
 local secretKey = "mySecretKey"
-local function encryptString(str, key)
-    local encrypted = syn.crypt.encrypt(str, key)
-    return encrypted
+local function encryptStringUsingSecretKey(str, key)
+    local encryptedString = syn.crypt.encrypt(str, key)
+    return encryptedString
 end
-local encryptedBytecode = encryptString(escapedBytecode, secretKey)
+local encryptedScriptBytecode = encryptStringUsingSecretKey(escapedScriptBytecode, secretKey)
 
 -- Generate a random key to use for obfuscation
-local function generateObfuscationKey()
+local function generateRandomObfuscationKey()
     math.randomseed(os.time())
     local key = ""
     for i = 1, 32 do
@@ -39,10 +39,10 @@ local function generateObfuscationKey()
     end
     return key
 end
-local obfuscationKey = generateObfuscationKey()
+local obfuscationKey = generateRandomObfuscationKey()
 
 -- Obfuscate the encrypted bytecode using the random key
-local function xorStrings(str1, str2)
+local function xorTwoStringsTogether(str1, str2)
     local result = ""
     for i = 1, #str1 do
         local byte1 = string.byte(str1, i)
@@ -51,7 +51,7 @@ local function xorStrings(str1, str2)
     end
     return result
 end
-local obfuscatedBytecode = xorStrings(encryptedBytecode, obfuscationKey)
+local obfuscatedScriptBytecode = xorTwoStringsTogether(encryptedScriptBytecode, obfuscationKey)
 
 -- Build the final script with string.format()
 local formatString = [[
@@ -59,11 +59,11 @@ local formatString = [[
 -- Bytecode Hash: %s
 loadstring(syn.crypt.decrypt(%q, %q))()
 ]]
-local decryptedBytecode = "syn.crypt.xor(" .. ("%q"):format(obfuscatedBytecode) .. ", " .. ("%q"):format(obfuscationKey) .. ")"
-local finalScript = string.format(formatString, syn.crypt.hash(obfuscatedBytecode), decryptedBytecode, secretKey)
+local decryptedScriptBytecode = "syn.crypt.xor(" .. ("%q"):format(obfuscatedScriptBytecode) .. ", " .. ("%q"):format(obfuscationKey) .. ")"
+local finalScriptContent = string.format(formatString, syn.crypt.hash(obfuscatedScriptBytecode), decryptedScriptBytecode, secretKey)
 
 -- Store the obfuscated script in the clipboard
-setclipboard(finalScript)
+setclipboard(finalScriptContent)
 
 -- Print the time taken to execute the script
 print(("Finished in %.2f seconds"):format(os.clock() - startTime))
